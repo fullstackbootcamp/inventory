4803a81bbeb62c309c23431f8e37c13a33562929
Marc Dagatan <marc.dagatan@gmail.com>
2020-02-08 17:06:56 +0800

    Update order of has_many :warehouses

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   has_many :stocks
-  has_many :warehouses, through: :stocks
   has_many :order_items
+  has_many :warehouses, through: :stocks
   has_many :orders, through: :order_items
 
   validates :name, presence: true
